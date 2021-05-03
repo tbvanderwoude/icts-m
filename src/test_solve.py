8cb7882d2939771922dd84b561288babd55d7d63
Thom van der Woude <tbvanderwoude@gmail.com>
2021-05-03 15:55:09 +0200

    Made test more compact using closure

--- src/test_solve.py
+++ src/test_solve.py
@@ -14,11 +14,8 @@ class SolveTestSingle(unittest.TestCase):
             [1, 0, 1, 0, 1],
             [1, 1, 1, 1, 1],
         ]
-
-    def test_solve_zero(self):
-        start: MarkedLocation = MarkedLocation(0, 1, 1)
-        goal: MarkedLocation = MarkedLocation(0, 1, 1)
-        problem: Problem = Problem(
+        def creator(start, goal):
+            return Problem(
             [
                 [1, 1, 1, 1, 1],
                 [1, 0, 1, 0, 1],
@@ -34,49 +31,25 @@ class SolveTestSingle(unittest.TestCase):
             1,
             1,
         )
+        self.instance_creator = creator
+
+    def test_solve_zero(self):
+        start: MarkedLocation = MarkedLocation(0, 1, 1)
+        goal: MarkedLocation = MarkedLocation(0, 1, 1)
+        problem: Problem = self.instance_creator(start,goal)
         solution = solve(problem)
         self.assertEqual(len(solution.paths[0].route), 2)
 
     def test_solve_one(self):
         start: MarkedLocation = MarkedLocation(0, 1, 1)
         goal: MarkedLocation = MarkedLocation(0, 1, 2)
-        problem: Problem = Problem(
-            [
-                [1, 1, 1, 1, 1],
-                [1, 0, 1, 0, 1],
-                [1, 0, 0, 0, 1],
-                [1, 0, 1, 0, 1],
-                [1, 1, 1, 1, 1],
-            ],
-            5,
-            5,
-            [start],
-            [goal],
-            0,
-            1,
-            1,
-        )
+        problem: Problem = self.instance_creator(start,goal)
         solution = solve(problem)
         self.assertEqual(len(solution.paths[0].route), 2)
 
     def test_solve_many(self):
         start: MarkedLocation = MarkedLocation(0, 1, 1)
         goal: MarkedLocation = MarkedLocation(0, 3, 3)
-        problem: Problem = Problem(
-            [
-                [1, 1, 1, 1, 1],
-                [1, 0, 1, 0, 1],
-                [1, 0, 0, 0, 1],
-                [1, 0, 1, 0, 1],
-                [1, 1, 1, 1, 1],
-            ],
-            5,
-            5,
-            [start],
-            [goal],
-            0,
-            1,
-            1,
-        )
+        problem: Problem = self.instance_creator(start,goal)
         solution = solve(problem)
         self.assertEqual(len(solution.paths[0].route), 5)
