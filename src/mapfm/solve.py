8aca91157ef2ad92719223d0552a5b92d9403217
Thom van der Woude <tbvanderwoude@gmail.com>
2021-05-04 17:42:06 +0200

    Added conflict avoidance by tie-breaking using conflict counts

--- src/mapfm/solve.py
+++ src/mapfm/solve.py
@@ -81,8 +81,7 @@ class Solver:
                 min_sic = sic
                 min_sol = sol.solution
                 self.update_budget(min_sic)
-            print("Solved matching")
-
+            # print("Solved matching")
         # print(min_sic)
         subsols = list(zip(*min_sol))
         for subsol in subsols:
