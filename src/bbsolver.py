d1228e75a26fe221243b12775a6b28ab4a21198c
Thom van der Woude <tbvanderwoude@gmail.com>
2021-08-18 12:14:42 +0200

    Implemented generation of matchings with lower bound from root problem

--- src/bbsolver.py
+++ src/bbsolver.py
@@ -1,5 +1,5 @@
 import heapq
-from typing import List, Tuple, Set
+from typing import List, Tuple, Set, Generator
 
 from mapfmclient import Problem
 
@@ -14,15 +14,40 @@ from branch_and_bound.assignment_problem import AssignmentProblem
 def evaluate(node: BBNode):
     return node.lower_bound + node.__hash__() % 100
 
+def murty_gen(costs, root):
+    ls: List[BBNode] = [root]
+    heapq.heapify(ls)
+    seen: Set[BBNode] = set()
+    while ls:
+        n: BBNode = heapq.heappop(ls)
+        if n not in seen: # and (not min_cost or n.lower_bound < min_cost):
+            seen.add(n)
+            if n.is_leaf():
+                # c = evaluate(n)
+                # print("{} Leaf node with lower-bound {} evaluated to {} (current upper: {})".format(index,n.lower_bound, c,
+                #                                                                                  min_cost))
+                yield n;
+                # index+=1
+                # if not min_cost or c < min_cost:
+                #     min_cost = c
+            else:
+                children = n.problem.generate_subproblems()
+                if len(children) == 1:
+                    heapq.heappush(ls, BBNode(n, children[0], n.lower_bound))
+                else:
+                    for sub_problem in n.problem.generate_subproblems():
+                        sub_cost = solve_problem(costs, sub_problem)
+                        heapq.heappush(ls, BBNode(n, sub_problem, sub_cost))
+
 def solve_bb(problem: Problem):
     # translates MAPFM problem to assignment problem (relaxation
     k = len(problem.starts)
-    costs = [[0 for j in range(k)] for i in range(k)]
+    costs = [[0 for _ in range(k)] for _ in range(k)]
     # makes sure that the K teams are numbered without gaps as 0...(K-1)
     reverse_map = enumerate(sorted(set(map(lambda x: x.color, problem.starts))))
     color_map = dict([(sub[1], sub[0]) for sub in reverse_map])
     K = len(color_map)
-    print(color_map)
+    # print(color_map)
     print("There are {} agents in {} teams".format(k,K))
     agents: List[MarkedCompactLocation] = list(
         map(
@@ -36,7 +61,7 @@ def solve_bb(problem: Problem):
             problem.goals,
         )
     )
-    print(agents,goals)
+    # print(agents,goals)
     maze: Maze = Maze(problem.grid, problem.width, problem.height)
     for (i,(al,ac)) in enumerate(agents):
         for (j,(gl,gc)) in enumerate(goals):
@@ -44,7 +69,7 @@ def solve_bb(problem: Problem):
                 shortest_path = astar(maze, al, gl)
                 c = len(shortest_path) - 1
                 costs[i][j] = c
-    print(costs)
+    # print(costs)
     # costs = [
     #     [90, 80, 75, 70, 10, 30],
     #     [35, 85, 55, 65, 50, 10],
@@ -54,39 +79,16 @@ def solve_bb(problem: Problem):
     #     [5, 20, 5, 15, 100, 15],
     # ]
     team_id = [x[1] for x in agents]
-    team_tasks =dict([(team, set([g[0] for g in enumerate(goals) if g[1][1] == team])) for team in range(K)])
+    team_tasks =[set([g[0] for g in enumerate(goals) if g[1][1] == team]) for team in range(K)]
 
-    print(team_id,team_tasks)
+    # print(team_id,team_tasks)
     root_problem = AssignmentProblem(team_id, team_tasks, K, k, k)
     root_cost = solve_problem(costs, root_problem)
     root = BBNode(None, root_problem, root_cost)
-    print(root_cost)
+    matching_generator = murty_gen(costs, root)
+    for bb_node in matching_generator:
+        print(bb_node.problem.assignments,bb_node.lower_bound)
     return None
-    # ls: List[BBNode] = [root]
-    # heapq.heapify(ls)
-    # seen: Set[BBNode] = set()
-    # min_cost = None
-    # index = 0
-    # while ls:
-    #     n: BBNode = heapq.heappop(ls)
-    #     if n not in seen and (not min_cost or n.lower_bound < min_cost):
-    #         seen.add(n)
-    #         if n.is_leaf():
-    #             c = evaluate(n)
-    #             print("{} Leaf node with lower-bound {} evaluated to {} (current upper: {})".format(index,n.lower_bound, c,
-    #                                                                                              min_cost))
-    #             index+=1
-    #             if not min_cost or c < min_cost:
-    #                 min_cost = c
-    #         else:
-    #             children = n.problem.generate_subproblems()
-    #             if len(children) == 1:
-    #                 heapq.heappush(ls, BBNode(n, children[0], n.lower_bound))
-    #             else:
-    #                 for sub_problem in n.problem.generate_subproblems():
-    #                     sub_cost = solve_problem(costs, sub_problem)
-    #                     heapq.heappush(ls, BBNode(n, sub_problem, sub_cost))
-    # return []
 
 
 # if __name__ == "__main__":
